6eeaf3900bb950fae87aa6968d609d8904286872
aidylewis <adrian_lewis@hushmail.com>
2019-02-13 16:00:38 +0000

    small refactor

--- lib/va.ex
+++ lib/va.ex
@@ -33,7 +33,27 @@ defmodule Va do
     Scribe.print(data, data: [:hostname, :ip])
 
     IO.puts("Open Ports")
-    Enum.filter(data.ports, fn %{output: output} -> output == [] end) |> Enum.map(fn x -> Map.delete(x, :output) end) |> Scribe.print()
+    data.ports
+    |> Enum.filter(fn %{output: output} -> output == [] end)
+    |> Scribe.print()
 
+    IO.puts("Vulnerabilities")
+    data = Enum.filter(data.ports, fn %{output: output} -> output != [] end)
+    Scribe.print(data, data: [:port, :name, :product, :state, :version])
+
+    [m] = data
+    %{output: output} = m
+
+    List.to_string(tl(output))
+    |> String.split("\n")
+    |> List.delete_at(0)
+    |> List.delete_at(0)
+    |> List.delete_at(0)
+    |> List.to_string
+    |> String.split("\t")
+    |> Enum.filter(fn x -> x != "" end)
+    |> Enum.chunk_every(3)
+    |> Enum.map(fn [cve, cvs, link] -> %{cve: cve, cvs: cvs, link: link} end)
+    |> Scribe.print()
   end
 end
